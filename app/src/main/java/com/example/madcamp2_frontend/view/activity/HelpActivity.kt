5aed17dc0c6ef9c98fa1093de9ba4b69f7d597aa
gyuch-an02 <gyuch.an02@kaist.ac.kr>
2024-07-10 16:06:30 +0900

    Fix: Help font 수정

--- app/src/main/java/com/example/madcamp2_frontend/view/activity/HelpActivity.kt
+++ app/src/main/java/com/example/madcamp2_frontend/view/activity/HelpActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.content.res.ResourcesCompat
 import com.example.madcamp2_frontend.R
 import com.example.madcamp2_frontend.databinding.ActivityHelpBinding
 
@@ -25,10 +26,9 @@ class HelpActivity : AppCompatActivity() {
     }
 
     private fun setupHelpItems() {
-        // 질문과 답변을 배열로 만듭니다.
         val questions = arrayOf(
             "Q. Drawdle은 무엇인가요?",
-            "Q. 한 시간에 두 번 이상 플레이할 수는 없나요?"
+            "Q. 한 시간에 두 번보다 많이 플레이할 수는 없나요?"
         )
 
         val answers = arrayOf(
@@ -36,7 +36,6 @@ class HelpActivity : AppCompatActivity() {
             "Drawdle은 매 시간마다 단어가 업데이트되지만, 각 단어 당 두 번의 기회를 줍니다.\n우리는 하루에 24 번, 모든 참여자가 동일한 정답을 맞추는 게임 경험을 제공합니다.\n정답 단어는 매 시각의 정각에 업데이트됩니다."
         )
 
-        // 질문과 답변을 동적으로 화면에 추가합니다.
         for (i in questions.indices) {
             val question = questions[i]
             val answer = answers[i]
@@ -44,8 +43,8 @@ class HelpActivity : AppCompatActivity() {
             val questionTextView = TextView(this)
             questionTextView.text = question
             questionTextView.textSize = 18f
-            questionTextView.setTypeface(null, Typeface.BOLD) // 질문을 굵은 글자로 설정
-            questionTextView.setPadding(0, 0, 0, 8) // Bottom padding
+            questionTextView.setTypeface(ResourcesCompat.getFont(this, R.font.bazzi), Typeface.BOLD)
+            questionTextView.setPadding(0, 0, 0, 8)
 
             val answerTextView = TextView(this)
             answerTextView.text = answer
